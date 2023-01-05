01361a4f49a6ae8c8d3e939215e0d3841a55d453
samsamgo <jku2659@gmail.com>
2023-01-05 15:02:24 +0900

    feat add redux toolkit

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from 'redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+/*redux 예시*/
+const num = 14;
+
+function reducer(state = num, action) {
+  if (action.type === 'increase') {
+    state = action.number;
+    return state;
+  } else if (action.type === 'decrease') {
+    state--;
+    return state;
+  } else if (action.type === 'ADD_TODO') {
+    return [];
+  }
+
+  return state;
+}
+
+let contain = createStore(reducer);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={contain}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 App();
