f9b7eb1f453b288b70a16393380b06f02cdae05d
euijin0122 <tricklet@naver.com>
2023-01-06 15:06:59 +0900

    feat: init

---
+++ server/seb41_main_031/src/main/java/com/codestates/main/seb41_main_031/Seb41Main031Application.java
@@ -0,0 +1,13 @@
+package com.codestates.main.seb41_main_031;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seb41Main031Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seb41Main031Application.class, args);
+	}
+
+}
---
+++ server/seb41_main_031/src/test/java/com/codestates/main/seb41_main_031/Seb41Main031ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.main.seb41_main_031;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seb41Main031ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
