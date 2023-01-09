8db6fd94890162566a488ef7967b3af4e5191553
euijin0122 <tricklet@naver.com>
2023-01-09 13:25:07 +0900

    init

---
+++ server/src/main/java/com/codestates/seb41_main_031/amuna/AmunaApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.seb41_main_031.amuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmunaApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/codestates/seb41_main_031/amuna/AmunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.seb41_main_031.amuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
