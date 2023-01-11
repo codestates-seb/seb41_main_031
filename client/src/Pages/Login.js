7ed86e8d2c60f13a07176a080e389d7f6d0c9c0f
samsamgo <jku2659@gmail.com>
2023-01-11 13:48:57 +0900

    Update Login.js

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import styled from "styled-components";
-<<<<<<< Updated upstream
 const LoginContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;  
+  height: 100vh;
 `;
 
 const Form = styled.form`
@@ -30,7 +29,7 @@ const Button = styled.button`
   color: white;
   border: 1px solid #ccc;
   border-radius: 4px;
-  background-color: rgba(30,30,255,0.75);
+  background-color: rgba(30, 30, 255, 0.75);
   &:hover {
     cursor: pointer;
     background-color: #ddd;
@@ -41,94 +40,100 @@ const LoginHeader = styled.h2`
   display: flex;
   justify-content: center;
   margin-bottom: 100px;
-`
+`;
 
 const HrefRight = styled.a`
-    display: flex;
-    justify-content: right;
-`
+  display: flex;
+  justify-content: right;
+`;
 
 const Login = function LoginForm() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
 
     // Check the entered username and password against a list of valid credentials
-    if (username === 'admin' && password === 'password') {
+    if (username === "admin" && password === "password") {
       // Redirect the user to the logged-in version of the app
-      window.location.replace('/MainPage');
+      window.location.replace("/MainPage");
     } else {
       // Display an error message to the user
-      setErrorMessage('아이디 혹은 비밀번호가 정확하지 않습니다');
+      setErrorMessage("아이디 혹은 비밀번호가 정확하지 않습니다");
     }
   }
- 
+
   return (
     <LoginContainer>
-    <Form onSubmit={handleSubmit}>
-    <LoginHeader> 
-    <h2>아무나에 오신 걸 환영합니다.<br />사람들과의 연결을 경험해보세요!</h2>
-    </LoginHeader> 
-    <span>이메일</span> 
+      <Form onSubmit={handleSubmit}>
+        <LoginHeader>
+          <h2>
+            아무나에 오신 걸 환영합니다.
+            <br />
+            사람들과의 연결을 경험해보세요!
+          </h2>
+        </LoginHeader>
+        <span>이메일</span>
         <Input
           type="text"
           value={username}
-          onChange={event => setUsername(event.target.value)}
+          onChange={(event) => setUsername(event.target.value)}
         />
-    <span>비밀번호</span>
+        <span>비밀번호</span>
         <Input
           type="password"
           value={password}
-          onChange={event => setPassword(event.target.value)}
+          onChange={(event) => setPassword(event.target.value)}
         />
-      
-      <br />
-      {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
-      <Button type="submit">로그인</Button>
-   <HrefRight><a href = "Signup"> 처음이신가요? 시작하기 </a> </HrefRight>
-    </Form>
+
+        <br />
+        {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
+        <Button type="submit">로그인</Button>
+        <HrefRight>
+          <a href="Signup"> 처음이신가요? 시작하기 </a>{" "}
+        </HrefRight>
+      </Form>
     </LoginContainer>
   );
-}
-    //admin과 password 입력하면 localhost:3000/dashboard로 이동함 
-    // send a request to your backend to check the user's credentials
-    // if the login is successful, you can redirect the user to another page or show a success message
-    // if the login fails, you can update the component's state to display an error message
-
-    export default Login;
-=======
-
-const LoginContainer = styled.div``;
-const LoginButtonColor = styled.button`
-  background-color: blue;
-  border-color: blue;
-`;
-function Login() {
-  return (
-    <LoginContainer>
-      <div>아무나에 오신 걸 환영합니다.</div>
-      <div>사람들과의 연결을 경험해보세요!</div>
-      <div>Email</div>
-      <input type="text" placeholder="이메일을 입력해주세요"></input>
-      <br></br>
-      <div>Password</div>
-      <input type="password" placeholder="비밀번호를 입력해주세요"></input>
-      <br></br>
-      <LoginButton />
-    </LoginContainer>
-  );
-}
-
-function LoginButton() {
-  return (
-    <LoginButtonColor>
-      <button id="LoginButton">Login</button>
-    </LoginButtonColor>
-  );
-}
+};
+//admin과 password 입력하면 localhost:3000/dashboard로 이동함
+// send a request to your backend to check the user's credentials
+// if the login is successful, you can redirect the user to another page or show a success message
+// if the login fails, you can update the component's state to display an error message
 
 export default Login;
->>>>>>> Stashed changes
+// =======
+
+// const LoginContainer = styled.div``;
+// const LoginButtonColor = styled.button`
+//   background-color: blue;
+//   border-color: blue;
+// `;
+// function Login() {
+//   return (
+//     <LoginContainer>
+//       <div>아무나에 오신 걸 환영합니다.</div>
+//       <div>사람들과의 연결을 경험해보세요!</div>
+//       <div>Email</div>
+//       <input type="text" placeholder="이메일을 입력해주세요"></input>
+//       <br></br>
+//       <div>Password</div>
+//       <input type="password" placeholder="비밀번호를 입력해주세요"></input>
+//       <br></br>
+//       <LoginButton />
+//     </LoginContainer>
+//   );
+// }
+
+// function LoginButton() {
+//   return (
+//     <LoginButtonColor>
+//       <button id="LoginButton">Login</button>
+//     </LoginButtonColor>
+//   );
+// }
+
+// export default Login;
+// >>>>>>> Stashed changes
