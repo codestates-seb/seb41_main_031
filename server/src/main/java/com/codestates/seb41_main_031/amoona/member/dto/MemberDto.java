b7cb2060857035b6e50cfe03f4b007a41ea4f01e
yjcms <sen9563@gmail.com>
2023-01-11 16:52:13 +0900

    feat: Member api layer

---
+++ server/src/main/java/com/codestates/seb41_main_031/amoona/member/dto/MemberDto.java
@@ -0,0 +1,56 @@
+package com.codestates.seb41_main_031.amoona.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.time.LocalDateTime;
+
+public class MemberDto {
+    // 회원 가입 dto
+    @AllArgsConstructor
+    @Getter
+    public static class Post {
+        @NotBlank
+        @Email
+        private String email;
+        @NotBlank
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+                message = "비밀번호는 최소 8자, 최소 하나의 문자 및 하나의 숫자를 포함해야 합니다.")
+        private String password;
+        @NotBlank(message = "닉네임은 공백이 아니어야 합니다.")
+        private String nickName;
+    }
+
+    // 회원 프로필 정보 수정 dto
+    @AllArgsConstructor
+    @Getter
+    public static class Patch {
+        private Long memberId;
+        private String region;
+        private String gender;
+        private int age;
+        private String image;
+
+        public void setMemberId(Long memberId) {
+            this.memberId = memberId;
+        }
+
+    }
+
+    // response dto
+    @AllArgsConstructor
+    @Getter
+    public static class Response {
+        private Long memberId;
+        private String email;
+        private String region;
+        private String image;
+        private String nickname;
+        private int age;
+        private LocalDateTime createdAt;
+        private LocalDateTime modifiedAt;
+    }
+}
---
+++ server/src/main/java/com/codestates/seb41_main_031/amoona/member/mapper/MemberMapper.java
@@ -0,0 +1,11 @@
+package com.codestates.seb41_main_031.amoona.member.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface MemberMapper {
+//    Member memberPostToMember(MemberDto.Post postRequest);
+//    Member memberPatchToMember(MemberDto.Patch patchRequest);
+//    MemberDto.Response memberToMemberResponse(Member member);
+//    List<MemberDto.Response> memberListToMemberResponseList(List<Member> memberList);
+}
