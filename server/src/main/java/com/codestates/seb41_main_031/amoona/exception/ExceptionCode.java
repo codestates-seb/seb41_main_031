af64517c5775ced336b873ffb8502c40bcb232f2
euijin0122 <tricklet@naver.com>
2023-01-19 18:11:44 +0900

    feat: Post 기능 구현

--- server/src/main/java/com/codestates/seb41_main_031/amoona/exception/ExceptionCode.java
+++ server/src/main/java/com/codestates/seb41_main_031/amoona/exception/ExceptionCode.java
@@ -28,17 +28,3 @@ public enum ExceptionCode {
         this.message = message;
     }
 }
-//    POST_NOT_FOUND(404, "Post Not Found"),
-//    MEMBER_NOT_FOUND(404, "Member Not Found"),
-//    MEMBER_NOT_ALLOWED(409, "Member Not Allowed");
-//
-//    @Getter
-//    private int status;
-//    @Getter
-//    private String message;
-//
-//    ExceptionCode(int status, String message) {
-//        this.status = status;
-//        this.message = message;
-//    }
-//}
